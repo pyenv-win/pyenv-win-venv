17ecd784520e3e1a63093830a99fbcdb0b3e4f6e
Arbaaz Laskar <arzkar.dev@gmail.com>
2023-03-31 18:54:20 +0530

    fix: Suppress redirect output

--- bin/pyenv-venv.bat
+++ bin/pyenv-venv.bat
@@ -1,2 +1,3 @@
+@ECHO OFF
 @REM Alias for pyenv-win-venv.bat
 "%USERPROFILE%\.pyenv-win-venv\bin\pyenv-win-venv.bat" %*
--- bin/pyenv-win-venv.bat
+++ bin/pyenv-win-venv.bat
@@ -1,1 +1,2 @@
+@ECHO OFF
 powershell -File "%USERPROFILE%\.pyenv-win-venv\bin\pyenv-win-venv.ps1" "bat" %*
